ae46fe064b0bd08d60aebc01e6c87edcd2eb7553
TirthDoshi2184 <tirthdoshi2184@gmail.com>
2026-03-02 13:17:40 +0530

    changes for 5173 port

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const cors = require('cors');
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'http://localhost:5173',
     'https://cosmic-virtue-frontend.vercel.app'
   ],
   credentials: true
