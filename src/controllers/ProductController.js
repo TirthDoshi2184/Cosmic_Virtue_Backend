f2db69cd6862372154f4b7767388641160a3c5de
TirthDoshi2184 <tirthdoshi2184@gmail.com>
2026-03-21 17:27:32 +0530

    re

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,6 +1,5 @@
 const ProductSchema = require('../models/ProductModel');
 // Add this at the top of ProductController.js
-
 const Category = require('../models/CategoryModel');
 
 // Create new product
