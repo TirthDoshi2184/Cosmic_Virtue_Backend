0910c129fbc391682d770194119d59f88c972cea
TirthDoshi2184 <tirthdoshi2184@gmail.com>
2026-04-05 17:23:00 +0530

    trying to solve the error

--- app.js
+++ app.js
@@ -27,9 +27,18 @@ app.use(cors({
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Database connected successfully'))
-  .catch((err) => console.error('Database connection error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+})
+.then(() => console.log('Database connected successfully'))
+.catch((err) => console.error('Database connection error:', err));
+
+// Add this to auto-reconnect
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected! Reconnecting...');
+  mongoose.connect(process.env.MONGO_URI);
+});
 
 const userRoutes = require('./src/routes/UserRoutes');
 const productRoutes = require('./src/routes/ProductRoutes');
