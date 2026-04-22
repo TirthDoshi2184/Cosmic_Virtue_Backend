9e6a97dfa393e6bbcbeabb03cd3eb4e255c92f29
Tirth Doshi <tirthdoshi2184@gmail.com>
2026-04-22 19:30:37 +0530

    firebase change

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,14 +1,30 @@
 const admin = require('firebase-admin');
+const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager');
 
-if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+let initialized = false;
+
+const initializeFirebase = async () => {
+  if (initialized || admin.apps.length) return;
+
+  const client = new SecretsManagerClient({ region: 'ap-south-1' });
   
+  const response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: 'cosmic-virtue/firebase-service-account',
+    })
+  );
+
+  const serviceAccount = JSON.parse(response.SecretString);
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-}
+
+  initialized = true;
+};
 
 exports.verifyFirebaseToken = async (token) => {
+  await initializeFirebase();
   const decoded = await admin.auth().verifyIdToken(token);
   return decoded;
 };
